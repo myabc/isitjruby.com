c64029a24ea805810f3da588073ac4a0111ab42b
Alex Coles <alex@alexcolesportfolio.com>
2009-12-14 15:23:40 +0100

    Update JRuby downloads URI for scraping version info
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- app/models/jruby_version.rb
+++ app/models/jruby_version.rb
@@ -2,13 +2,13 @@ require 'open-uri'
 
 class JrubyVersion < ActiveRecord::Base
   def self.versions
-    uri = URI.parse("http://dist.codehaus.org/jruby/")
+    uri = URI.parse("http://jruby.kenai.com/downloads/")
     uri.read.scan(/\"\d+\.\d+.*\/\"/).map { |v| v.gsub(/[\/\"]/, "") }.sort
   end
 
   def self.load_versions
     JrubyVersion.all.each { |v| v.delete }
-    
+
     self.versions.each do |v|
       JrubyVersion.create :version => v
     end
